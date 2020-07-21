d7595ce65fb096839921bf7c9cfd7a49bc27c425
Craig Jones <craig@strongdm.com>
2020-07-21 13:14:13 -0500

    2_managing_accounts: add create_user example

---
+++ 2_managing_accounts/create_user/src/CreateUser.java
@@ -0,0 +1,58 @@
+
+// Copyright 2020 StrongDM Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+import java.util.concurrent.TimeUnit;
+
+import com.strongdm.api.v1.*;
+
+public class CreateUser {
+    public static void main(String[] args) {
+        // Load the SDM API keys from the environment.
+        // If these values are not set in your environment,
+        // please follow the documentation here:
+        // https://www.strongdm.com/docs/admin-guide/api-credentials/
+        var apiAccessKey = System.getenv("SDM_API_ACCESS_KEY");
+        var apiSecretKey = System.getenv("SDM_API_SECRET_KEY");
+        if (apiAccessKey == null || apiSecretKey == null) {
+            System.out.println("SDM_API_ACCESS_KEY and SDM_API_SECRET_KEY must be provided");
+            return;
+        }
+
+        try {
+            // Create the SDM Client
+            var opts = new ClientOptions().withHostAndPort("api.strongdmdev.com", 443);
+            var client = new Client(apiAccessKey,apiSecretKey, opts);
+
+            // Define a user
+            var user = new User();
+            user.setEmail("example@example.com");
+            user.setFirstName("example");
+            user.setLastName("example");
+            
+            // Create the user
+            var response = client.accounts().create(user);
+            var userResponse = (User)response.getAccount();
+
+
+            System.out.println("Successfully created user.");
+            System.out.printf("     ID: %s\n", userResponse.getId());
+            System.out.printf("  Email: %s\n", userResponse.getEmail());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        
+    }
+}
