ed95702207fa9426f732900f95e2eaf192f3f1c5
Phil Hassey <phil@galcon.com>
2020-07-28 11:23:03 -0600

    1_managing_resources: rename update datasource example to update resource

--- 1_managing_resources/update_resource/src/UpdateResource.java
+++ 1_managing_resources/update_resource/src/UpdateResource.java
@@ -16,7 +16,7 @@ import java.util.concurrent.TimeUnit;
 
 import com.strongdm.api.v1.*;
 
-public class UpdateDatasource {
+public class UpdateResource {
     public static void main(String[] args) {
         // Load the SDM API keys from the environment.
         // If these values are not set in your environment,
