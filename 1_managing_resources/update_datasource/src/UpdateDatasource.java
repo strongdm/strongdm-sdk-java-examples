9a7d4888a2cf1206f1e417571a91d9df8abb09af
Phil Hassey <phil@galcon.com>
2020-07-28 11:07:50 -0600

    1_managing_resources: add update datasource example

---
+++ 1_managing_resources/update_datasource/src/UpdateDatasource.java
@@ -0,0 +1,77 @@
+// Copyright 2020 StrongDM Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+import java.util.concurrent.TimeUnit;
+
+import com.strongdm.api.v1.*;
+
+public class UpdateDatasource {
+    public static void main(String[] args) {
+        // Load the SDM API keys from the environment.
+        // If these values are not set in your environment,
+        // please follow the documentation here:
+        // https://www.strongdm.com/docs/admin-guide/api-credentials/
+        var apiAccessKey = System.getenv("SDM_API_ACCESS_KEY");
+        var apiSecretKey = System.getenv("SDM_API_SECRET_KEY");
+        if (apiAccessKey == null || apiSecretKey == null) {
+            System.out.println("SDM_API_ACCESS_KEY and SDM_API_SECRET_KEY must be provided");
+            return;
+        }
+
+        try {
+            // Create the SDM Client
+            var opts = new ClientOptions();
+            var client = new Client(apiAccessKey,apiSecretKey, opts);
+
+            // Define a Postgres Datasource
+            var postgres = new Postgres();
+            postgres.setName("Example Postgres Datasource");
+            postgres.setHostname("example.strongdm.com");
+            postgres.setPort(5432);
+            postgres.setUsername("example");
+            postgres.setPassword("example");
+            postgres.setDatabase("example");
+            postgres.setPortOverride(19999);
+            
+            // Create the datasource
+            var createResponse = client.resources()
+                .withDeadlineAfter(30, TimeUnit.SECONDS)
+                .create(postgres);
+
+            System.out.println("Successfully created Postgres datasource.");
+            System.out.printf("    ID:%s\n", createResponse.getResource().getId());
+            System.out.printf("  Name:%s\n", createResponse.getResource().getName());
+
+            // Load the datasource to update
+            var getResponse = client.resources()
+                .withDeadlineAfter(30, TimeUnit.SECONDS)
+                .get(createResponse.getResource().getId());
+            var updatedPostgresDatasource = getResponse.getResource();
+
+            // Update the fields to change
+            updatedPostgresDatasource.setName("Updated Example Name");
+
+            // Update the datasource
+            var updateResponse = client.resources()
+                .withDeadlineAfter(30, TimeUnit.SECONDS)
+                .update(updatedPostgresDatasource);
+
+            System.out.println("Successfully updated Postgres datasource.");
+            System.out.printf("    ID:%s\n", updateResponse.getResource().getId());
+            System.out.printf("  Name:%s\n", updateResponse.getResource().getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
