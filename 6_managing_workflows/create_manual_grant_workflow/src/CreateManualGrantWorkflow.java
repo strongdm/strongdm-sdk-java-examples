40e8dde2f70315e7aedd9f9cb901eb395eaab6de
johnnyhchen <johnnychenv@gmail.com>
2023-09-19 11:07:29 -0700

    6_managing_workflows: update create examples
    Addresses: OTR-784

--- 6_managing_workflows/create_manual_grant_workflow/src/CreateManualGrantWorkflow.java
+++ 6_managing_workflows/create_manual_grant_workflow/src/CreateManualGrantWorkflow.java
@@ -1,4 +1,3 @@
-
 // Copyright 2023 StrongDM Inc
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,12 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-import java.util.concurrent.TimeUnit;
+package samples;
+
+import java.util.Map;
+
+import javax.management.relation.Role;
 
 import com.strongdm.api.v1.*;
 
-public class DeleteWorkflow {
-    public static void main(String[] args) {
+public class CreateManualGrantWorkflow {
+    public static void main(String[] args) throws Exception {
         // Load the SDM API keys from the environment.
         // If these values are not set in your environment,
         // please follow the documentation here:
@@ -29,28 +32,33 @@ public class DeleteWorkflow {
             System.out.println("SDM_API_ACCESS_KEY and SDM_API_SECRET_KEY must be provided");
             return;
         }
+        
 
         try {
             // Create the SDM Client
             var opts = new ClientOptions();
             var client = new Client(apiAccessKey, apiSecretKey, opts);
 
-            // Define a Workflow
-            var create = new Workflow();
-            create.setName("Example Workflow");
+            // Create an access rule for the workflow
+            AccessRule accessRule = new AccessRule();
+            Map<String, String> tags = new Hashmap<>();
+            tags.put("env", "dev");
+            accessRule.setTags(tags);
 
-            // Create the Workflow
-            var createResponse = client.workflows().create(create);
-            var workflow = createResponse.getWorkflow();
+            // Create a Workflow
+            Workflow workflow = new Workflow();
+            workflow.setName("Example Create Manual Grant Worfklow");
+            workflow.setDescription("Example Workflow Description");
+            workflow.setAccessRule(accessRule);
+
+            workflow = client.workflows().create(workflow).getWorkflow();
+
             System.out.println("Successfully created workflow.");
             System.out.printf("\tID: %s\n", workflow.getId());
             System.out.printf("\tName: %s\n", workflow.getName());
-
-            // Delete the Workflow
-            client.workflows().delete(workflow.getId());
-            System.out.println("Successfully deleted workflow.");
         } catch (Exception e) {
             e.printStackTrace();
         }
+        
     }
 }
---
+++ 6_managing_workflows/create_workflows_full_examples/create_auto_grant_workflow/src/CreateAutoGrantWorkflow.java
@@ -0,0 +1,85 @@
+// Copyright 2023 StrongDM Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package samples;
+
+import java.util.Map;
+
+import com.strongdm.api.v1.*;
+
+public class CreateManualGrantWorkflow {
+    public static void main(String[] args) throws Exception {
+        // Load the SDM API keys from the environment.
+        // If these values are not set in your environment,
+        // please follow the documentation here:
+        // https://www.strongdm.com/docs/api/api-keys/
+        var apiAccessKey = System.getenv("SDM_API_ACCESS_KEY");
+        var apiSecretKey = System.getenv("SDM_API_SECRET_KEY");
+        if (apiAccessKey == null || apiSecretKey == null) {
+            System.out.println("SDM_API_ACCESS_KEY and SDM_API_SECRET_KEY must be provided");
+            return;
+        }
+        
+
+        try {
+            // Create the SDM Client
+            var opts = new ClientOptions();
+            var client = new Client(apiAccessKey, apiSecretKey, opts);
+
+            // Create an access rule for the workflow
+            AccessRule accessRule = new AccessRule();
+            Map<String, String> tags = new Hashmap<>();
+            tags.put("env", "dev");
+            accessRule.setTags(tags);
+
+            // Create a Workflow
+            Workflow workflow = new Workflow();
+            workflow.setName("Example Create Auto Grant Worfklow");
+            workflow.setDescription("Example Workflow Description");
+            workflow.setAutoGrant(true);
+            workflow.setEnabled(true);
+            workflow.setAccessRule(accessRule);
+
+            workflow = client.workflows().create(workflow).getWorkflow();
+
+            System.out.println("Successfully created workflow.");
+            System.out.printf("\tID: %s\n", workflow.getId());
+            System.out.printf("\tName: %s\n", workflow.getName());
+
+            // To allow users access to the resources managed by this workflow, you must
+            // add workflow roles to the workflow.
+            // Two steps are needed to add a workflow role:
+            // Step 1: create a Role
+            // Step 2: create a WorkflowRole
+
+            // Create a Role
+            Role role = new Role();
+            role.setName("Example Role for Auto Grant Workflow");
+            role = client.roles().create(role).getRole();
+
+            // Create a workflow role
+            WorkflowRole workflowRole = new WorkflowRole();
+            workflowRole.workflowId = workflow.getId();
+            workflowRole.roleId = role.getId();
+            workflowRole = client.workflowRoles().create(workflowRole).getWorkflowRole();
+            
+            System.out.println("Successfully created workflow role.");
+            System.out.printf("\tWorkflow ID: %s\n", workflowRole.getWorkflowId());
+            System.out.printf("\tRole ID: %s\n", workflowRole.getRoleId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
